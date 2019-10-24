78a35e3dbf0b2784541ca35c938061b8c1b356bc
AndyOmondi17 <andyomondi17@gmail.com>
2019-10-24 09:06:20 +0300

    add create.sql file

---
+++ create.sql
@@ -0,0 +1,25 @@
+
+CREATE DATABASE wildlife;
+\c wildlife;
+
+
+CREATE TABLE IF NOT EXISTS animals(
+id SERIAL PRIMARY KEY,
+sighting_id INTEGER,
+animal_name VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS endangeredAnimals(
+id SERIAL PRIMARY KEY,
+animal_name VARCHAR,
+animal_age VARCHAR,
+animal_health VARCHAR,
+sighting_id INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS sightings(
+id SERIAL PRIMARY KEY,
+animal_location VARCHAR,
+ranger_name VARCHAR,
+sighted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
